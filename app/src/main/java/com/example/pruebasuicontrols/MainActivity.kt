02f4613370bffa6af0b81ce9e415141d59700b03
Yago Gonzalez <yagogonzalezamat@gmail.com>
2023-10-06 11:36:42 +0200

    clase 6 oct 2023

--- app/src/main/java/com/example/pruebasuicontrols/MainActivity.kt
+++ app/src/main/java/com/example/pruebasuicontrols/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.pruebasuicontrols
 
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,11 +16,27 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.pruebasuicontrols.ui.theme.PruebasUIControlsTheme
 
 class MainActivity : ComponentActivity() {
+    private lateinit var nombre: EditText
+    private lateinit var pass: EditText
+    private lateinit var respuesta: TextView
+    private lateinit var respuesta1: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             setContentView(R.layout.main_activity)
+            nombre = findViewById(R.id.nombre)
+            pass = findViewById(R.id.contraseña)
+            respuesta = findViewById(R.id.respuesta)
+            respuesta1 = findViewById(R.id.respuesta1)
             }
         }
+
+    fun mostrarTexto(view: View){
+        val nombre = nombre.text.toString()
+        val contra = pass.text.toString()
+        respuesta.text = nombre
+        respuesta1.text = contra
+
+    }
     }
 
